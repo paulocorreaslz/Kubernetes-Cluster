20996211c79413988499b5d0d130e3f9cca2355a
Paulo Correa <pauloyaco@gmail.com>
2021-03-06 23:10:50 -0300

    add interal ip output

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,16 +3,30 @@
 # retorna o ip interno associado ao droplet wsteste
 #digitalocean_droplet.wsteste01.ipv4_address_private
 
+
+
 output "ip-ext-vm-01-k8s-master" {
     value = digitalocean_droplet.vm-01-k8s-master.ipv4_address   
+}
+
+output "ip-int-vm-01-k8s-master" {
+    value = digitalocean_droplet.vm-01-k8s-master.ipv4_address_private 
 }
 
 output "ip-ext-vm-02-k8s-master" {
     value = digitalocean_droplet.vm-02-k8s-master.ipv4_address   
 }
 
+output "ip-int-vm-02-k8s-master" {
+    value = digitalocean_droplet.vm-02-k8s-master.ipv4_address_private 
+}
+
 output "ip-ext-vm-03-k8s-master" {
     value = digitalocean_droplet.vm-03-k8s-master.ipv4_address   
+}
+
+output "ip-int-vm-03-k8s-master" {
+    value = digitalocean_droplet.vm-03-k8s-master.ipv4_address_private 
 }
 
 output "ip-ext-vm-04-k8s-worker" {
